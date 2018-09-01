85bab62bde123d53d6f3f7739a4a59cd0f9eab10
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-01 18:21:07 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,3 @@
-## `Person` and `lib/person.rb`
-
-### Give a Person a Name
-
-You'll be teaching `Person` about their names through two methods, `#name`, and `#name=` that read and write to a corresponding instance variable `@name`.
-
-### Give a Person a Job
-
-You'll be teaching `Person` about their jobs through two methods, `#job`, and `#job=` that read and write to a corresponding instance variable `@job`.
-
 class Person
 
   def name=(the_persons_name)
